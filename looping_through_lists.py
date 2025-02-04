5020ce73812846438f8197627c0facba557256d2
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-04 20:51:49 +0300

    Add looping through lists example in looping_through_lists.py

---
+++ looping_through_lists.py
@@ -0,0 +1,5 @@
+#Chapter 4: Looping through lists
+magicians_names = ['alice', 'david', 'carolina','Harry', 'Houdini']
+for magician_name in magicians_names:
+    print(f'{magician_name.title()}, that was a great trick!')
+    print(f'I cannot wait to see your next trick, {magician_name.title()}.\n')
