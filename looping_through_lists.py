bafe711d68e5e12413cbcf0ab4049ab75d27ce3e
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-05 12:55:22 +0300

    Enhance looping_through_lists.py to include a closing message after the magic show

--- looping_through_lists.py
+++ looping_through_lists.py
@@ -1,5 +1,7 @@
 #Chapter 4: Looping through lists
 magicians_names = ['alice', 'david', 'carolina','Harry', 'Houdini']
-for magician_name in magicians_names:
-    print(f'{magician_name.title()}, that was a great trick!')
+for magician_name in magicians_names: #Created a temporary variable magician_name to hold the value of the list
+    print(f'{magician_name.title()}, that was a great trick!') #Printed the value of the temporary variable
     print(f'I cannot wait to see your next trick, {magician_name.title()}.\n')
+print('Thank you everyone, that was a great magic show!') #Printed a message after the loop ends
+   
