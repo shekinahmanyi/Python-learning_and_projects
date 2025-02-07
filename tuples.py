34b6118cf692ea432546f1e3d625143e902cca22
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-07 12:34:30 +0300

    Add example demonstrating tuple usage and modification in tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+# Tuples
+dimensions = (200, 50) # Tuples are immutable lists and the difference is that they use parentheses instead of square brackets
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100) # You can assign a new value to a variable that holds a tuple
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
