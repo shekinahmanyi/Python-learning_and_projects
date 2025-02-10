b51fd1ebc6b7c1ce2237224970838b32375e9554
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-10 13:49:59 +0300

    Update if_statements.py to demonstrate checking for inequality and modify existing examples

--- if_statements.py
+++ if_statements.py
@@ -1,7 +1,12 @@
 # if Statements
-cars = ['audi', 'bmw', 'subaru', 'toyota']
-for car in cars:
-    if car == 'bmw':
-        print(car.upper())
-    else:
-        print(car.title())
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+# for car in cars:
+#     if car == 'audi': # check if the first value in the list is 'audi' and if it is, print it in uppercase
+#         print(car.upper())
+#     else:
+#         print(car.title())
+#Checking for Inequality
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
