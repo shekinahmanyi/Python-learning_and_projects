d01114d3e8233a14dd8e666badc15bd6eed2fc92
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-11 13:26:18 +0300

    Add toppings.py to demonstrate conditional statements with pizza toppings

---
+++ toppings.py
@@ -0,0 +1,9 @@
+requested_topping = ['mushrooms', 'onions', 'pineapple']
+if 'mushrooms' in requested_topping:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_topping:
+    print("Adding pepperoni.")
+elif 'onions' in requested_topping:
+    print("Adding onions.")
+
+print("\nFinished making your pizza!")
