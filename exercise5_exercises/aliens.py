c24d1505b1762133b460f7c94596f622abb0aa9e
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-11 13:26:29 +0300

    Add aliens.py to demonstrate conditional statements with alien colors and scoring

---
+++ exercise5_exercises/aliens.py
@@ -0,0 +1,12 @@
+#Exercise 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+alien_color = ['green', 'yellow', 'red']
+
+if 'green' in alien_color:
+    print("You just earned 5 points!")
+if 'yellow' in alien_color:
+    print("You just earned 10 points!")
+if 'blue' in alien_color:
+    print("You just earned 15 points!")
+
+print("\nFinished playing the game!")
