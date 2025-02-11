8d48c160812e85e7313478c779c335df0d670637
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-11 13:26:08 +0300

    Update if_statements.py to include age-based admission pricing using if-elif-else statements

--- if_statements.py
+++ if_statements.py
@@ -34,12 +34,22 @@
 # print(car == 'audi')
 
 #If-Else Statements
-age = 12
-if age >= 18:
-    print("You are old enough to vote!")
-    print("Have you registered to vote yet?")
-else:
-    print("Sorry, you are too young to vote.")
-    print("Please register to vote as soon as you turn 18!")
+# age = 12
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
 
 #The if-elif-else Chain
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price =25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+print(f"Your admission cost is ${price}.")
