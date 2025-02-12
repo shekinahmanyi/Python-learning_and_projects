3d82bbc9551dce0e56f8aaa09fc189779cd4e584
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-12 08:40:42 +0300

    Add user check to hello_admin.py to handle empty user list

--- exercise5_exercises/hello_admin.py
+++ exercise5_exercises/hello_admin.py
@@ -4,9 +4,22 @@
 #•	Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 #Exercise 5-8: Hello Admin
 
-usernames = ['admin', 'eric', 'james', 'john', 'jane']
-for username in usernames:
-    if username == 'admin':
-        print("Hello admin, would you like to see a status report?")
-    else:
+# usernames = ['admin', 'eric', 'james', 'john', 'jane']
+# for username in usernames:
+#     if username == 'admin':
+#         print("Hello admin, would you like to see a status report?")
+#     else:
+#         print("Hello " + username.title() + ", thank you for logging in again.")
+
+#Exercise 5-9: No Users
+#Add an if test to hello_admin.py to make sure the list of users is not empty.
+#•	If the list is empty, print the message We need to find some users!
+
+usernames = []
+if usernames:
+    for username in usernames:
         print("Hello " + username.title() + ", thank you for logging in again.")
+else:
+        print("We need to find some users!")
+
+       
