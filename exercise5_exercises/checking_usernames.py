56e00c593f7acf5f85d2ad1b34da3244866697d9
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-12 08:46:35 +0300

    Add checking_usernames.py to simulate unique username validation

---
+++ exercise5_exercises/checking_usernames.py
@@ -0,0 +1,16 @@
+#Checking Usernames Exercise
+#Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+#•	Make a list of five or more usernames called current_users.
+#•	Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+#•	Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+#•	Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. (To do this, you’ll need to make a copy of current_users containing the lowercase versions of all existing users.)
+
+current_users = ['admin', 'eric', 'james', 'john', 'jane']
+new_users = ['jane', 'james', 'jane', 'john', 'jane']
+
+current_users_lower = [user.lower() for user in current_users]
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print(f"Sorry, {new_user} is already taken. Please enter a new username.")
+    else:
+        print(f"{new_user} is available.")
