1bd2d6f23336b74233dbf7c4f536bbff06b97a32
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-13 12:39:59 +0300

    Add dictionaries.py to demonstrate basic dictionary operations and key-value manipulation

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+#Understanding Dictionaries
+
+aliens = {'color': 'green', 'points': 5} # this aliens has 2 key-value pairs color and points
+
+print(aliens['color']) # we are accessing the value of color key
+print(aliens['points']) # we are accessing the value of points key
+
+aliens['x_position'] = 0 # adding a new key-value pair to the dictionary
+aliens['y_position'] = 25 # adding a new key-value pair to the dictionary
+
+print(aliens)
+
