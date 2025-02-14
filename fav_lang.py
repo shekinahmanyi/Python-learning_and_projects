97d0f7cc11cc0e741d9d2dbf89c4f58b493ba73a
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-14 13:12:57 +0300

    Add fav_lang.py to demonstrate dictionary usage with favorite programming languages

---
+++ fav_lang.py
@@ -0,0 +1,11 @@
+
+# A dictionary of similar objects
+fav_lang = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+language = fav_lang['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
