b197861dd403347ac227121e5067db01d0d71883
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-16 16:45:00 +0300

    Enhance person.py to include additional print statements for all key-value pairs

--- chapter_6_exercises/person.py
+++ chapter_6_exercises/person.py
@@ -1,6 +1,10 @@
 #Exercise 6.1 Person
 person = {'first_name': 'John', 'last_name': 'Doe', 'age': 25, 'city': 'New York'} # this person has 4 key-value pairs first_name, last_name, age and city
+
 print(person['first_name'])
+
 print(person['last_name'])
+
 print(person['age'])
+
 print(person['city'])
