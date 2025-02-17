1ec8d28e173f1f88e7a138e0375f602ec083b496
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-17 12:56:53 +0300

    Add favourite_numbers.py to demonstrate dictionary usage for storing favorite numbers

---
+++ chapter_6_exercises/favourite_numbers.py
@@ -0,0 +1,11 @@
+#Using a Dictionary to store people's favorite numbers
+fav_numbers = {
+    'jen': 7,
+    'sarah': 3,
+    'edward': 5,
+    'phil': 9,
+}
+print(f"Jen's favorite number is {fav_numbers['jen']}")
+print(f"Sarah's favorite number is {fav_numbers['sarah']}")
+print(f"Edward's favorite number is {fav_numbers['edward']}")
+print(f"Phil's favorite number is {fav_numbers['phil']}")
