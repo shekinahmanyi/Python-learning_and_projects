cb7b6dfe888165ab3b15416233d8ad532acdeae2
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-19 17:58:04 +0300

    Format users dictionary for improved readability

--- users.py
+++ users.py
@@ -1,4 +1,8 @@
-users= {'username': 'efermi', 'first': 'enrico', 'last': 'fermi'}
+users= {
+    'username': 'efermi', 
+    'first': 'enrico', 
+    'last': 'fermi'
+    }
 
 for key, value in users.items():
     print(f"\nKey: {key}")
