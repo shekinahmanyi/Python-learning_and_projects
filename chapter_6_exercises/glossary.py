29a92114d02aa1d46dae27e91e2a84817868d4e4
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-19 18:07:52 +0300

    Refactor glossary.py to improve readability and utilize a loop for printing definitions

--- chapter_6_exercises/glossary.py
+++ chapter_6_exercises/glossary.py
@@ -1,10 +1,16 @@
 #Glossary
-fav_programming_words = {'list': 'A collection of items in a particular order', 'tuples': 'A collection of items that cannot be changed', 'dictionary': 'A collection of key-value pairs', 'string': 'A series of characters', 'comments': 'A note in a program that the Python interpreter ignores', 'loop': 'Work through a collection of items, one at a time', 'conditional test': 'A comparison between two values', 'boolean expression': 'An expression that evaluates to True or False', 'key-value pair': 'A set of values associated with each other', 'float': 'A numerical value with a decimal component'}
-print(f"List is {fav_programming_words['list']}")
-print(f"Tuples is {fav_programming_words['tuples']}")
-print(f"Dictionary is {fav_programming_words['dictionary']}")
-print(f"String is {fav_programming_words['string']}")
-print(f"Comments is {fav_programming_words['comments']}")
-print(f"Loop is {fav_programming_words['loop']}")
-print(f"Conditional test is {fav_programming_words['conditional test']}")
-print(f"Boolean expression is {fav_programming_words['boolean expression']}")
+fav_programming_words = {'list': 'A collection of items in a particular order',
+                          'tuples': 'A collection of items that cannot be changed',
+                           'dictionary': 'A collection of key-value pairs', 
+                            'string': 'A series of characters', 
+                            'comments': 'A note in a program that the Python interpreter ignores',
+                            'loop': 'Work through a collection of items, one at a time',
+                            'conditional test': 'A comparison between two values',
+                             'boolean expression': 'An expression that evaluates to True or False', 
+                            'key-value pair': 'A set of values associated with each other',
+                            'float': 'A numerical value with a decimal component'}
+
+#using a loop to print out the key-value pairs
+for word, definition in fav_programming_words.items():
+    print(f"{word.title()}: {definition}")
+
