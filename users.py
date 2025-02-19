e7c358744c278417dce6b9fd22254e6ebb57793b
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-19 17:02:14 +0300

    Refactor users.py to consolidate user data structure and improve iteration

--- users.py
+++ users.py
@@ -1,9 +1,5 @@
-user_0 = {
-    'username': 'efermi',
-    'first': 'enrico',
-    'last': 'fermi',
-    }
+users= {'username': 'efermi', 'first': 'enrico', 'last': 'fermi'}
 
-for key, value in user_0.items():
+for key, value in users.items():
     print(f"\nKey: {key}")
     print(f"Value: {value}")
