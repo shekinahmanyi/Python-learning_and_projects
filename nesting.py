d1d1726744626248adc92db64aee0647a8b39596
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-25 08:03:29 +0300

    Modify alien attributes for the first three aliens in nesting.py to enhance diversity in color, speed, and points

--- nesting.py
+++ nesting.py
@@ -15,6 +15,18 @@ for alien_number in range(30): # range() function returns a series of numbers.
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'} # Create a new alien.
     aliens.append(new_alien) # Add the new alien to the list.
 
+    # Make the first 3 aliens yellow.
+    for alien in aliens[:3]:
+        if alien['color'] == 'green':
+            alien['color'] = 'yellow'
+            alien['speed'] = 'medium'
+            alien['points'] = 10
+    # Make the first 3 aliens red.
+        elif alien['color'] == 'yellow':
+           alien['color'] = 'red'
+           alien['speed'] = 'fast'
+           alien['points'] = 15
+
 
 # Show the first 5 aliens.
 for alien in aliens[:5]: # Slice the list to print the first 5 aliens.
