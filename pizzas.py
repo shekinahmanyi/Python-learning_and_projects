225a41116e9f3374119396e3324992579aa75d73
shekinahmanyi <shekinahmanyi@gmail.com>
2025-02-27 19:08:45 +0300

    Refactor pizzas.py to improve comments and enhance clarity in pizza order summary

--- pizzas.py
+++ pizzas.py
@@ -1,11 +1,11 @@
 # Store Information about a pizza being ordered.
-pizza = {
+pizza = { 
     'crust': 'thick',
-    'toppings': ['mushrooms', 'extra cheese'],
+    'toppings': ['mushrooms', 'extra cheese'], # created a list of toppings in a dictionary
 }
 # Summarize the order.
-print(f"You ordered a {pizza['crust']}-crust pizza "
+print(f"You ordered a {pizza['crust']}-crust pizza " # Accessing the value of 'crust' key and we are using f-string to format the output.
       "with the following toppings:")
-for topping in pizza['toppings']:
+for topping in pizza['toppings']: # Accessing the value of 'toppings' key.
     print("\t" + topping)
-    
+
