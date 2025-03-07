022abe5980d0cfb9d1e962a967c05a8723f587a6
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-07 05:54:24 +0300

    Refactor parrots.py to remove height check and implement user input echo functionality

--- parrots.py
+++ parrots.py
@@ -1,7 +1,18 @@
-height = input("How tall are you, in inches? ")
-height = int(height)
 
-if height >= 36:
-    print("\nYou're tall enough to ride!")
-else:
-    print("\nYou'll be able to ride when you're a little older.")
+
+# height = input("How tall are you, in inches? ")
+# height = int(height)
+
+# if height >= 78:
+#     print("\nYou're tall enough to ride!")
+# else:
+#     print("\nYou'll be able to ride when you're a little older.")
+
+#letting the user choose when to quit
+prompt = "\nTell me something, and I will repeat it back to you:"   
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
