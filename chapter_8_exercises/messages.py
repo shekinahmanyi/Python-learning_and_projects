a101757f177e39d201a1e352c300d98d8dbec127
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-08 15:04:51 +0300

    Add display_message function to announce learning progress in Python

---
+++ chapter_8_exercises/messages.py
@@ -0,0 +1,8 @@
+#writing a function that tells everyone that I am learning Python
+
+def display_message(username):
+
+    print(f"Hello Everyone, {username.title()} is Learning Python and she is enjoying Functions!")
+
+display_message('Shekinah')
+
