9e5b31db18539c59e20e93441cdf360bf7b37b7c
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-08 14:49:38 +0300

    Add greet_user function to display a simple greeting

---
+++ functions.py
@@ -0,0 +1,6 @@
+# Functions
+
+def greet_user(): # Here we are defining a function called greet_user()
+    """" Display a simple greeting """ # This is called a Docstring and it describes what the function does.
+print("Hello")
+greet_user()
