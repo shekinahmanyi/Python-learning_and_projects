3076473b1ab12e0e2936502ef0b888c8c3d61566
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-08 14:59:31 +0300

    Update greet_user function to accept a username parameter and display a personalized greeting

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 # Functions
 
-def greet_user(): # Here we are defining a function called greet_user()
-    """" Display a simple greeting """ # This is called a Docstring and it describes what the function does.
-print("Hello")
-greet_user()
+def greet_user(username): # Here we are defining a function called greet_user()
+
+    # """" Display a simple greeting """ # This is called a Docstring and it describes what the function does.
+
+    print(f"Hello, {username.title()}!")
+    
+greet_user('Shekinah')
