c01bd9d489ccde619054537de0ac36a8daac2a89
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-13 08:44:19 +0300

    Using positional arguments to pass arguments

---
+++ functions/pets.py
@@ -0,0 +1,8 @@
+
+def describe_pets(animal_type, pet_name):
+
+    """ Display Information about a pet """
+    print(f"\nI have an {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name}.")
+
+describe_pets('hamster', 'harry')
