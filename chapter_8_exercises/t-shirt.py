70a81bb87dea5aa6a83023ba243e07845f15d0b6
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-17 12:33:31 +0300

    Refactor t_shirt function to set default size and update function calls for consistency

--- chapter_8_exercises/t-shirt.py
+++ chapter_8_exercises/t-shirt.py
@@ -1,9 +1,9 @@
 
 #This is the exercise on t-shirt
 
-def t_shirt(size, text) :
+def t_shirt( text , size='l') :
     """ Display Information about this t-shirt"""
     print(f"You wear size {size} and {text}")
 
-t_shirt("Large", "Jesus Loves you!")
-t_shirt(size='Medium', text='We Pray for a Living')
+t_shirt(text='I love Python')
+t_shirt(size='Medium', text='I Pray for a Living')
