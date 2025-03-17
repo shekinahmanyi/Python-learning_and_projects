aa627ac01c3fa9c3ef18db7df7b080e0d9fde2f3
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-17 12:57:22 +0300

    Add function to format full names and demonstrate usage

---
+++ functions/formatted_name.py
@@ -0,0 +1,11 @@
+
+def get_formatted_name(first_name, last_name):
+
+    """Return a Full name, neatly formatted"""
+    full_name = f"{first_name} {last_name}"
+
+    return full_name.title()
+
+musician = get_formatted_name('Dunsin', 'Oyekan')
+
+print(musician)
