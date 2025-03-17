a0d603d5710eaf58340de9c3db5e1cc1931aa6c1
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-17 11:58:58 +0300

    Update describe_pets function calls to use keyword arguments for clarity

--- functions/pets.py
+++ functions/pets.py
@@ -5,5 +5,5 @@ def describe_pets(animal_type, pet_name):
     print(f"\nI have an {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name}.")
 
-describe_pets('hamster', 'harry')
-describe_pets('dog', 'William')
+describe_pets(animal_type='hamster', pet_name='harry')
+# describe_pets('dog', 'William') #here this can take as many parameters as needed
