03de5556d2bc97328ab364ae445a728d048c39d6
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-17 12:22:34 +0300

    Add t-shirt exercise with function to display size and text

---
+++ chapter_8_exercises/t-shirt.py
@@ -0,0 +1,9 @@
+
+#This is the exercise on t-shirt
+
+def t_shirt(size, text) :
+    """ Display Information about this t-shirt"""
+    print(f"You wear size {size} and {text}")
+
+t_shirt("Large", "Jesus Loves you!")
+t_shirt(size='Medium', text='We Pray for a Living')
