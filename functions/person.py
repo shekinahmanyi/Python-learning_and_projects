509edee81bc3c373dcbb01f452fa9952c7882d01
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-18 20:15:23 +0300

    Add function to build a person's dictionary representation

---
+++ functions/person.py
@@ -0,0 +1,11 @@
+#RETURNING A DICTIONARY
+
+def build_person(first_name, last_name):
+
+    """Return a dictionary of information about a person"""
+
+    person = {'first':first_name, 'last':last_name}
+    return person
+
+musician = build_person('Dunsin', 'Oyekan')
+print(musician)
