6a04008e1834a2a37d252bdf5359f6e8e0563929
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-18 19:55:08 +0300

    Add optional middle name parameter to get_formatted_name function

--- functions/formatted_name.py
+++ functions/formatted_name.py
@@ -1,9 +1,11 @@
 
-def get_formatted_name(first_name, last_name): #Here we are defining a function cal get_formatted_name
+def get_formatted_name(first_name, last_name, middle_name=''): #Here we are defining a function cal get_formatted_name and making the middle name optional
 
     """Return a Full name, neatly formatted""" #Docstring to tell what the function does
-    full_name = f"{first_name} {last_name}"   #we are defining a variable that combines both the first name and the last name
-
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"   #we are defining a variable that combines both the first name and the last name
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title() #we now return the full name
 
 #Sample cases on how to get the full name of various people with their different roles
@@ -11,7 +13,7 @@ def get_formatted_name(first_name, last_name): #Here we are defining a function 
 musician = get_formatted_name('Dunsin', 'Oyekan')
 print(musician)
 
-writer = get_formatted_name('Shekinah', 'Manyi')
+writer = get_formatted_name('Shekinah', 'Manyi','Achidi')
 print(writer)
 
 farmer = get_formatted_name('Giren', 'Grace')
