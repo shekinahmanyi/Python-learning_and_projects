e23370dbf9025c11c9c0f5a989e5b7253fc7891d
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-20 13:03:38 +0300

    Add city_country function to return formatted string of country and city

---
+++ chapter_8_exercises/city.py
@@ -0,0 +1,11 @@
+#Exercises
+
+def city_country(country, city):
+
+    """" This Function Returns a String of Country and City """
+
+    full_country_name = f"The Country is '{country}'\n The City is '{city}'"
+    return full_country_name
+
+first_country = city_country('Cameroon', 'Douala')
+print(first_country)
