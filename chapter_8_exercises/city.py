0764fee644b206c22fb90360179aa4f6f567f17e
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-20 13:07:47 +0300

    Enhance city_country function to improve output formatting and add examples

--- chapter_8_exercises/city.py
+++ chapter_8_exercises/city.py
@@ -4,8 +4,14 @@ def city_country(country, city):
 
     """" This Function Returns a String of Country and City """
 
-    full_country_name = f"The Country is '{country}'\n The City is '{city}'"
-    return full_country_name
+    full_country_name = f"The Country is '{country}' and the City is '{city}'"
+    return full_country_name.title()
 
 first_country = city_country('Cameroon', 'Douala')
 print(first_country)
+
+second_country = city_country('Nigeria', 'Lagos')
+print(second_country)
+
+third_country = city_country('USA', 'california')
+print(third_country)
