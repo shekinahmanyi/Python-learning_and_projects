a4b463fa861180a998a3c18a76214c94169bd04f
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-20 12:06:02 +0300

    Add optional age parameter to build_person function

--- functions/person.py
+++ functions/person.py
@@ -1,11 +1,13 @@
 #RETURNING A DICTIONARY
 
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age=None): #Here we are making Age optional
 
     """Return a dictionary of information about a person"""
 
     person = {'first':first_name, 'last':last_name}
+    if age:
+        person['age'] = age
     return person
 
-musician = build_person('Dunsin', 'Oyekan')
+musician = build_person('Dunsin', 'Oyekan', age=40)
 print(musician)
