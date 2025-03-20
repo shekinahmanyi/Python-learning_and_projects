ed237e61c2bb959d52d221466fb10cb17ae35d69
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-20 15:50:58 +0300

    Add make_albums function to create album information dictionary

---
+++ chapter_8_exercises/album.py
@@ -0,0 +1,22 @@
+
+#This Exercise is from chapter 8 which is functions
+
+def make_albums(artist_name, artist_title, number_of_songs=None):
+
+    """" Return a dictionary of Information about the album"""
+
+    name_of_artist = {'Artist Name':artist_name, 'Song Title':artist_title}
+
+    if number_of_songs :
+        name_of_artist['Number of Songs Released'] = number_of_songs
+
+    return name_of_artist
+
+first_album = make_albums('Dunsin Oyekan','Who is like you', 100)
+print(first_album)
+
+second_album = make_albums('Dunsin Oyekan', 'Yah', 50)
+print(second_album)
+
+third_album = make_albums('Dunsin Oyekan', 'Judah', 150)
+print(third_album)
