06f14df0631e8df337cc6d2ff7a48ab10aa8d29b
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-26 19:51:15 +0300

    Add comment to clarify *toppings parameter in make_pizza function

--- functions/pizza.py
+++ functions/pizza.py
@@ -1,5 +1,6 @@
 
-def make_pizza(*toppings):
+#This syntax works no matter how many parameter the function receives
+def make_pizza(*toppings): #The asterisk tells python to make an empty tuple called toppings and pack whatever values it receives from the tuple
 
     """"Print the list of toppings that have been requested"""
     print("\nMaking a pizza with the following toppings:")
