1fa309c3d1413e33b1d549bd80e3e3547b27605f
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-26 19:59:29 +0300

    Add size parameter to make_pizza function and update print statement

--- functions/pizza.py
+++ functions/pizza.py
@@ -1,12 +1,12 @@
 
 #This syntax works no matter how many parameter the function receives
-def make_pizza(*toppings): #The asterisk tells python to make an empty tuple called toppings and pack whatever values it receives from the tuple
+def make_pizza(size,*toppings): #The asterisk tells python to make an empty tuple called toppings and pack whatever values it receives from the tuple
 
     """"Print the list of toppings that have been requested"""
-    print("\nMaking a pizza with the following toppings:")
-
+    print(f"\nMaking a {size}-inch pizza with the following toppings:") 
+   
     for topping in toppings:
         print(f"- {topping}")
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni') 
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
