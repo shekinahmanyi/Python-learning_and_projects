5550bcb3b354efdf5765bfc77f7e32bb41352e6e
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-26 19:27:36 +0300

    Update show_completed_models function to includena copy of unprinted designs in output

--- functions/printing_models.py
+++ functions/printing_models.py
@@ -22,7 +22,7 @@ def show_completed_models(completed_models):
     """"Show all the models that were printed."""
     print("\nThe following models have been printed:")
     for completed_model in completed_models:
-        print(completed_model) 
+        print(unprinted_designs[:], completed_model) 
 
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
