26d49e15f8eb0903cd25832fb18819d77ed1becd
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-28 10:22:51 +0300

    Remove example calls from make_pizza function to clean up code

--- functions/pizza.py
+++ functions/pizza.py
@@ -7,6 +7,3 @@ def make_pizza(size,*toppings): #The asterisk tells python to make an empty tupl
    
     for topping in toppings:
         print(f"- {topping}")
-
-make_pizza(16, 'pepperoni') 
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
