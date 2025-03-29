a91ddd1f7b1636eb88a30074972c8e4018980068
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-29 14:57:05 +0300

    Add instance creation and method calls for Dog class

--- classes/dog.py
+++ classes/dog.py
@@ -25,4 +25,20 @@ class Dog: #We are creating a class called Dog
 
         print(f"{self.name} rolled over")
 
+my_dog = Dog("Willie", 6) #Creating an instance of the Dog class called my_dog
+your_dog = Dog('Lucy', 3)
+
+print(f"My dog's name is {my_dog.name}.") #Printing the name of the dog
+print(f"My dog is {my_dog.age} years old.") #Printing the age of the dog
+
+
+my_dog.sit() #Calling the sit method on the my_dog instance
+my_dog.roll_over() #Calling the roll_over method on the my_dog instance
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit() #Calling the sit method on the your_dog instance
+your_dog.roll_over() #Calling the roll_over method on the your_dog instance
+
+
 
