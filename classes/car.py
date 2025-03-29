c78d8d9a684b17b2a3b61b155ecaac8af8f3921c
shekinahmanyi <shekinahmanyi@gmail.com>
2025-03-29 16:16:54 +0300

    Add method to update odometer with rollback prevention in Car class

--- classes/car.py
+++ classes/car.py
@@ -16,9 +16,20 @@ class Car :
     def read_odometer(self):
         """Print a statement showing the car's mileage."""
         print(f"This car has {self.odometer_reading} miles on it.")
-    
+
+    def update_odometer(self, mileage): #Modifying an attribute value through a method
+        # """Set the odometer reading to the given value."""
+        # self.odometer_reading = mileage 
+        """Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back."""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
 my_new_car = Car('audi', 'a4', 2023)
 print(my_new_car.get_descriptive_name())
 
+my_new_car.update_odometer(-20) # Trying to roll back the odometer
+my_new_car.update_odometer(23) # Setting the odometer to a new value
 my_new_car.read_odometer()
-my_new_car.odometer_reading = 23
