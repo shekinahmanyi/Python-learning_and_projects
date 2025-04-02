ee15aa24fb59a32325f655276da1c6e3be02e795
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-02 15:20:37 +0300

    Add support for middle names in get_formatted_name function

--- functions/name_function.py
+++ functions/name_function.py
@@ -1,7 +1,11 @@
 
-def get_formatted_name(first, last):
+def get_formatted_name(first, last , middle=''):
 
     """Generate a neatly formatted full name."""
-    
-    full_name = f"{first} {last}"
+
+    if middle:  # Check if a middle name is provided
+        full_name = f"{first} {middle} {last}"
+
+    else:  # If no middle name is provided, just use first and last names
+        full_name = f"{first} {last}"
     return full_name.title()
