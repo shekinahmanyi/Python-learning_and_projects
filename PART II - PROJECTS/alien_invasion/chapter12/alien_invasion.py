ccfed728b2e35766437b0a8266afd51988cd2613
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-03 22:36:01 +0300

    Initialize game resources in AlienInvasion class

--- PART II - PROJECTS/alien_invasion/chapter12/alien_invasion.py
+++ PART II - PROJECTS/alien_invasion/chapter12/alien_invasion.py
@@ -5,3 +5,12 @@ import pygame # type: ignore
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
+    
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = (230, 230, 230)
+        self.stats = None
