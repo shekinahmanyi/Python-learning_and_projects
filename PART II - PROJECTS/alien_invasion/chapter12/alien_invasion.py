de9e2ec5339c2024ee05e56a5f7383eb90135f82
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-06 22:03:24 +0300

    Refactor initialization in AlienInvasion class for improved readability

--- PART II - PROJECTS/alien_invasion/chapter12/alien_invasion.py
+++ PART II - PROJECTS/alien_invasion/chapter12/alien_invasion.py
@@ -5,8 +5,9 @@ import pygame # type: ignore
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
-    
+
     def __init__(self):
+        
         """Initialize the game, and create game resources."""
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
