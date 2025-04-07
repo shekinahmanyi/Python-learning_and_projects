6302400f86f5372cfeac55c53d457f7e818eadbe
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-07 20:22:31 +0300

    Add Settings class to manage game configuration for Alien Invasion

---
+++ PROJECTS/alien_invasion/chapter12/settings.py
@@ -0,0 +1,9 @@
+
+class Settings :
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self) :
+        """Initialize the game's static settings."""
+        #Screen settings
+        self.screen_width = 1200 #Width of the screen in pixels.
+        self.screen_height = 800 #Height of the screen in pixels.
+        self.bg_color = (230, 230, 230) #Background color of the screen in RGB format.
