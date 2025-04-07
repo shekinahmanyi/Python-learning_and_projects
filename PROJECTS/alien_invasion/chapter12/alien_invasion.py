605d6897e825e1b302f1347b195cd63e1a704a1c
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-07 20:47:38 +0300

    Refactor AlienInvasion class to utilize Settings class for screen dimensions and background color

--- PROJECTS/alien_invasion/chapter12/alien_invasion.py
+++ PROJECTS/alien_invasion/chapter12/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 
 import pygame #This module is used to create the game window and handle events.
 
+from settings import Settings #Import the Settings class from the settings module.
 class AlienInvasion:
 
     """Overall class to manage game assets and behavior."""
@@ -10,16 +11,14 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init() #Initializes the background settings that Pygame needs to run.
         #The argument is a tuple that defines the width and height of the window.
-        #The window will be 1200 pixels wide and 800 pixels tall.
         #We then assign the display window to the self.screen attribute so that it will be available to other methods in the class.
         #The object assigned to self.screen is a Surface object, which represents the game window.
         #The Surface returned by display.set_mode() represents the entire game window where we can draw graphics and display images.
-        self.screen = pygame.display.set_mode((1200, 800)) 
+        self.settings = Settings() 
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)) #This settings comes from the Settings class.
         pygame.display.set_caption("Alien Invasion") #Sets the title of the window to "Alien Invasion".
-
-        #Set the background color of the screen to a light gray color.
-        #The RGB color value (230, 230, 230) represents a light gray color.
-        self.bg_color = (230, 230, 230) #This attribute will be used to fill the screen with a background color.
+        
 
     def run_game(self): #This game is controlled by the run_game() method, which is the main loop of the game.
         """Start the main loop for the game."""
@@ -32,7 +31,7 @@ class AlienInvasion:
                     #If the event is a QUIT event, we call sys.exit() to exit the program.
                     sys.exit()
                     #Redraw the screen with the background color.
-                    self.screen.fill(self.bg_color)
+                    self.screen.fill(self.settings.bg_color) #comes from the Settings class.
                     #The fill() method fills the entire screen with the specified color.
             #make the most recently drawn screen visible
             pygame.display.flip() #This method updates the contents of the display window to show the most recently drawn screen.
