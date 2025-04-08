5fbfe06813bdb0290d8f6d63d37a0159940f5903
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-08 20:42:07 +0300

    Update cSpell configuration and enhance Ship class documentation

--- PROJECTS/alien_invasion/chapter12/ship.py
+++ PROJECTS/alien_invasion/chapter12/ship.py
@@ -2,9 +2,11 @@
 import pygame 
 
 class Ship:
+    
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
+
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
