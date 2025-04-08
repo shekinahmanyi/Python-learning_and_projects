765444dd830815285fbfe99ba2debcb168941322
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-08 20:13:27 +0300

    made code more readable

--- PROJECTS/alien_invasion/chapter12/settings.py
+++ PROJECTS/alien_invasion/chapter12/settings.py
@@ -1,6 +1,8 @@
 
 class Settings :
+
     """A class to store all settings for Alien Invasion."""
+    
     def __init__(self) :
         """Initialize the game's static settings."""
         #Screen settings
