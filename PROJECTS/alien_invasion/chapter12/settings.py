dfa4416d161c047c58fff3821f688115d17e1385
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-14 19:03:14 +0300

    Remove unnecessary blank lines in settings initialization for consistency

--- PROJECTS/alien_invasion/chapter12/settings.py
+++ PROJECTS/alien_invasion/chapter12/settings.py
@@ -4,7 +4,9 @@ class Settings :
     """A class to store all settings for Alien Invasion."""
     
     def __init__(self) :
+
         """Initialize the game's static settings."""
+        
         #Screen settings
         self.screen_width = 1200 #Width of the screen in pixels.
         self.screen_height = 800 #Height of the screen in pixels.
