c5910482cf93d95828f575106ca8a6aad996fd75
shekinahmanyi <shekinahmanyi@gmail.com>
2025-04-17 17:24:31 +0300

    Add ship speed setting and update movement logic

--- PROJECTS/alien_invasion/chapter12/settings.py
+++ PROJECTS/alien_invasion/chapter12/settings.py
@@ -11,3 +11,6 @@ class Settings :
         self.screen_width = 1200 #Width of the screen in pixels.
         self.screen_height = 800 #Height of the screen in pixels.
         self.bg_color = (230, 230, 230) #Background color of the screen in RGB format.
+
+        #Ship settings
+        self.ship_speed = 1.5
--- PROJECTS/alien_invasion/chapter12/ship.py
+++ PROJECTS/alien_invasion/chapter12/ship.py
@@ -8,14 +8,18 @@ class Ship:
 
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
-        # Load the ship image and get its rect.
+        self.settings = ai_game.settings #This gets the settings from the AlienInvasion class.
+        self.screen_rect = ai_game.screen.get_rect() #This gets the rectangle that represents the screen dimensions.
+       
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom #positioning the ship at the bottom center of the screen.
+
+        #store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
 
         #Movement flag.
         self.moving_right = False #This is a flag that indicates whether the ship is moving to the right or not.
@@ -25,12 +29,16 @@ class Ship:
     def update(self):
         """Update the ship's position based on the movement flag."""
         #Update the ship's position based on the movement flags.
+        #update the ship's x-coordinate based on the movement flags.
         if self.moving_right and self.rect.right < self.screen_rect.right: #This checks if the ship is moving to the right and if it is within the screen boundaries.
             #If the ship is moving to the right and is within the screen boundaries, we update its position by adding 1 to its x-coordinate.
-            self.rect.x += 1
+             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0: #This checks if the ship is moving to the left and if it is within the screen boundaries.
             #If the ship is moving to the left and is within the screen boundaries, we update its position by subtracting 1 from its x-coordinate.
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        #Update rect object from self.x.
+        self.rect.x = self.x
     
     def blitme(self): #This draws the image to the screen at the ship's current location.
         """Draw the ship at its current location."""
