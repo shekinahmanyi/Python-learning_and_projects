aa3b82e82f4a14386921089a544e76b496936ab4
shekinahmanyi <shekinahmanyi@gmail.com>
2026-08-31 13:00:41 +0300

    Add comment to bullet color setting for clarity

--- PROJECTS/alien_invasion/chapter12/settings.py
+++ PROJECTS/alien_invasion/chapter12/settings.py
@@ -19,4 +19,4 @@ class Settings :
         self.bullet_speed = 3.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (60, 60, 60) #Color of the bullet in RGB format.
